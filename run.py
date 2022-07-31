5c2827a38d9b71e128603f4ae09069788f69a8d0
IlyaIliev05 <ilievilia15@gmail.com>
2022-07-31 22:44:23 +0000

    Add helper method for ship placement in grid

--- run.py
+++ run.py
@@ -54,3 +54,33 @@ def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
             for c in range(start_col, end_col):
                 grid[r][c] = "O"
     return all_valid
+
+
+def try_place_ship_on_grid(row, col, direction, length):
+    """
+    Based on direction will call helper method to try and place a ship on grid
+    """
+    global grid_size
+
+    start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
+    if direction == "left":
+        if col - length < 0:
+            return False
+        start_col = col - length + 1
+
+    elif direction == "right":
+        if col + length >= grid_size:
+            return False
+        end_col = col + length
+
+    elif direction == "up":
+        if row - length < 0:
+            return False
+        start_row = row - length + 1
+
+    elif direction == "down":
+        if row + length >= grid_size:
+            return False
+        end_row = row + length
+
+    return validate_grid_and_place_ship(start_row, end_row, start_col, end_col)    
